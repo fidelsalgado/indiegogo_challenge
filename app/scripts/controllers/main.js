afbd46af0ebaebba94ec7d0e6cc4dbf9d64cb3bb
Fidel Salgado <fidels@stanford.edu>
2015-11-23 13:15:59 -0800

    Scaffolded using yeoman angular generator

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name indiegogoChallengeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the indiegogoChallengeApp
+ */
+angular.module('indiegogoChallengeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
