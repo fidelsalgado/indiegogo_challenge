53aff85d06cbab6dd66a27a7a6c5f9639a063178
Fidel Salgado <fidels@stanford.edu>
2015-11-23 14:48:23 -0800

    Called indiegogo API and render the info

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,19 @@
  * Controller of the indiegogoChallengeApp
  */
 angular.module('indiegogoChallengeApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $resource) {
+    var API_url = 'https://api.indiegogo.com/1/campaigns.json?api_token=';
+    var token = 'e377270bf1e9121da34cb6dff0e8af52a03296766a8e955c19f62f593651b346';
+
+    $resource(API_url + token).get(function(data) {
+      $scope.campaigns = data.response.map(function(obj) {
+        return {
+          title: obj.title,
+          tagline: obj.tagline,
+          image: obj.baseball_card_image_url,
+          url: obj.web_url
+        };
+      });
+    });
+
   });
